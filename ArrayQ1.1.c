0587bcbbc0ba8c559eb7ee2b16c08e8f0c3c11a5
shruty251-wq <shruty251@gmail.com>
2026-01-16 11:29:54 -0500

    Create ArrayQ1.1.c

---
+++ ArrayQ1.1.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+int main(){
+    int a[5];
+    int i ;
+    printf("Enter 5 element");
+    for (i= 0; i < 5 ; i = i+1){
+        scanf("%d",&a[i]);
+    }
+    printf("array,s elemnts");
+    for(i = 0 ; i <5 ; i = i + 1 ){
+        printf("%d ",a[5]);
+    }
+
+return 0;
+
+}
