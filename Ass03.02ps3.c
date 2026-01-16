4bb023a630b595ea3d500628e225315450efaa2c
shruty251-wq <shruty251@gmail.com>
2026-01-16 11:42:28 -0500

    Create Ass03.02ps3.c

---
+++ Ass03.02ps3.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+
+int main() {
+    float temp;
+    printf("Enter the temperature in centigrade: ");
+    scanf("%f", &temp);
+    if (temp < 0) {
+        printf("Freezing weather\n");
+    }
+    else if (temp >= 0 && temp < 10) {
+        printf("Very Cold weather\n");
+    }
+    else if (temp >= 10 && temp < 20) {
+        printf("Cold weather\n");
+    }
+    else if (temp >= 20 && temp < 30) {
+        printf("Normal in Temp\n");
+    }
+    else if (temp >= 30 && temp < 40) {
+        printf("It's Hot\n");
+    }
+    else {
+        printf("It's Very Hot\n");
+    }
+
+    return 0;
+}
