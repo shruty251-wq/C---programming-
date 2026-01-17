c6ce6b3ac5c06b2b539b6d6f18e8f8fdd54f9ac9
shruty251-wq <shruty251@gmail.com>
2026-01-16 23:25:55 -0500

    Create Ass03.04ps4.c

---
+++ Ass03.04ps4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int month;
+    printf("Enter month number (1-12): ");
+    scanf("%d", &month);
+    switch(month) {
+        case 1:  
+        case 3:  
+        case 5:  
+        case 7:  
+        case 8: 
+        case 10:
+        case 12: 
+            printf("31 days\n");
+            break;
+        case 4:  
+        case 6:  
+        case 9:  
+        case 11: 
+            printf("30 days\n");
+            break;
+        case 2:
+            printf("28 or 29 days (depending on leap year)\n");
+            break;
+        default:
+            printf("Invalid input! Please enter a number between 1 and 12.\n");
+    }
+
+    return 0;
+}
